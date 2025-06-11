c3ce84e839115329623a8354b898a4b466a4a401
Pablo <pablogc2233@gmail.com>
2025-06-11 09:15:43 +0200

    Generar Xml con los campos de las facturas

--- app/Console/Commands/GenerarFacturaXml.php
+++ app/Console/Commands/GenerarFacturaXml.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Facturas;
+use App\Services\FacturaXmlGenerator;
 
 class GenerarFacturaXml extends Command
 {
@@ -11,20 +13,34 @@ class GenerarFacturaXml extends Command
      *
      * @var string
      */
-    protected $signature = 'app:generar-factura-xml';
+    // Este es el comando para generar el Xml(php artisan factura:xml ID12345)
+    protected $signature = 'factura:xml {id}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Generar Xml de la factura';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $id = $this->argument('id');
+        $factura = Facturas::find($id);
+
+        if(!$factura){
+            $this ->error("Factura con el ID $id no fue encontrada");
+            return;
+        }
+
+        $generator = new FacturaXmlGenerator();
+        $xml = $generator->generateXml($factura);
+
+        $ruta = storage_path("app/factura_$id.xml");
+        file_put_contents($ruta, $xml);
+        $this->info("El Xml fue generado correctamente en: $ruta");
     }
 }
--- app/Services/FacturaXmlGenerator.php
+++ app/Services/FacturaXmlGenerator.php
@@ -36,12 +36,47 @@ class FacturaXmlGenerator
         $facturaElement->appendChild($dom->createElement('numSerieFacturaSustituida', $factura->numSerieFacturaSustituida));
         $facturaElement->appendChild($dom->createElement('fechaExpedicionFacturaSustituida', $factura->fechaExpedicionFacturaSustituida));
         $facturaElement->appendChild($dom->createElement('baseRectificada', $factura->baseRectificada));
-        //Datos del destinatario
+        $facturaElement->appendChild($dom->createElement('cuotaRectificada', $factura->cuotaRectificada));
+        $facturaElement->appendChild($dom->createElement('cuotaRecargoRectificado', $factura->cuotaRecargoRectificado));
+        $facturaElement->appendChild($dom->createElement('fechaOperacion', $factura->fechaOperacion));
+        $facturaElement->appendChild($dom->createElement('descripcionOperacion', $factura->descripcionOperacion));
+        $facturaElement->appendChild($dom->createElement('facturaSimplificadaArt7273', $factura->facturaSimplificadaArt7273));
+        $facturaElement->appendChild($dom->createElement('facturaSinIdentifDestinatarioArt61d', $factura->facturaSinIdentifDestinatarioArt61d));
+        $facturaElement->appendChild($dom->createElement('macrodato', $factura->macrodato));
+        $facturaElement->appendChild($dom->createElement('emitidaPorTerceroODestinatario', $factura->emitidaPorTerceroODestinatario));
         
+        //Datos del destinatario
+        $facturaElement->appendChild($dom->createElement('nombreRazon', $factura->nombreRazon));
+        $facturaElement->appendChild($dom->createElement('nif', $factura->nif));
+        $facturaElement->appendChild($dom->createElement('codigoPais', $factura->codigoPais));
+        $facturaElement->appendChild($dom->createElement('idType', $factura->idType));
+        $facturaElement->appendChild($dom->createElement('id', $factura->id));
+
         //Datos fiscales
-
+        $facturaElement->appendChild($dom->createElement('cupon', $factura->cupon));
+        $facturaElement->appendChild($dom->createElement('impuesto', $factura->impuesto));
+        $facturaElement->appendChild($dom->createElement('claveRegimen', $factura->claveRegimen));
+        $facturaElement->appendChild($dom->createElement('calificacionOperacion', $factura->calificacionOperacion));
+        $facturaElement->appendChild($dom->createElement('operacionExenta', $factura->operacionExenta));
+        $facturaElement->appendChild($dom->createElement('tipoImpositivo', $factura->tipoImpositivo));
+        $facturaElement->appendChild($dom->createElement('baseImponibleOimporteNoSujeto', $factura->baseImponibleOimporteNoSujeto));
+        $facturaElement->appendChild($dom->createElement('baseImponibleACoste', $factura->baseImponibleACoste));
+        $facturaElement->appendChild($dom->createElement('cuotaRepercutida', $factura->cuotaRepercutida));
+        $facturaElement->appendChild($dom->createElement('tipoRecargoEquivalencia', $factura->tipoRecargoEquivalencia));
+        $facturaElement->appendChild($dom->createElement('cuotaRecargoEquivalencia', $factura->cuotaRecargoEquivalencia));
+        $facturaElement->appendChild($dom->createElement('cuotaTotal', $factura->cuotaTotal));
+        $facturaElement->appendChild($dom->createElement('importeTotal', $factura->importeTotal));
+        $facturaElement->appendChild($dom->createElement('primerRegistro', $factura->primerRegistro));
+        
         //Registro adicional
+        $facturaElement->appendChild($dom->createElement('huella', $factura->huella));
+        $facturaElement->appendChild($dom->createElement('fechaHoraHusoGenRegistro', $factura->fechaHoraHusoGenRegistro));
+        $facturaElement->appendChild($dom->createElement('numRegistroAcuerdoFacturacion', $factura->numRegistroAcuerdoFacturacion));
+        $facturaElement->appendChild($dom->createElement('idAcuerdoSistemaInformatico', $factura->idAcuerdoSistemaInformatico));
+        $facturaElement->appendChild($dom->createElement('tipoHuella', $factura->tipoHuella));
+        
+        $dom->appendChild($facturaElement);
 
-
+        return $dom->saveXML();
     }
 }
