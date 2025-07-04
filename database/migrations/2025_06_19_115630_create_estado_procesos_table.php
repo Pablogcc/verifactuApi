d373a7f4b1c0eb7d51648614a84831d3a0f9766c
Pablo <pablogc2233@gmail.com>
2025-07-04 13:48:22 +0200

    Más cambios en la base de datos / Falta cambiar commands, controllers y servicios

--- database/migrations/2025_06_19_115630_create_estado_procesos_table.php
+++ database/migrations/2025_06_19_115630_create_estado_procesos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('estado_procesos', function (Blueprint $table) {
-            $table->id();
+              $table->id();
             $table->string('idVersion');
             $table->string('idInstalacion');
             $table->string('idEmisorFactura');
@@ -37,21 +37,22 @@ return new class extends Migration
             $table->decimal('cuotaRecargoRectificado', 10, 2);
             $table->string('fechaOperacion');
             $table->string('descripcionOperacion');
-            $table->string('facturaSimplificadaArt7273');
-            $table->string('facturaSinIdentifDestinatarioArt61d');
-            $table->string('macrodato');
-            $table->string('emitidaPorTerceroODestinatario');
+            $table->string('facturaSimplificadaArt7273')->default('N');
+            $table->string('facturaSinIdentifDestinatarioArt61d')->default('N');
+            $table->string('macrodato')->default('N');
+            $table->string('emitidaPorTerceroODestinatario')->default('N');
+            $table->string('huellaAnterior');
             // Datos del destinatario
             $table->string('nombreCliente');
             $table->string('nifCliente');
             $table->string('codigoPais');
-            $table->string('idType');
+            $table->string('idType')->default('NIF');
             // Datos fiscales
             $table->string('cupon');
-            $table->string('impuesto');
+            $table->string('impuesto')->default('IVA');
             $table->string('claveRegimen');
             $table->string('calificacionOperacion');
-            $table->string('operacionExenta');
+            $table->string('operacionExenta')->default('N');
             $table->decimal('tipoImpositivo', 10, 2);
             $table->decimal('tipoImpositivo2', 10, 2)->nullable();
             $table->decimal('tipoImpositivo3', 10, 2)->nullable();
@@ -76,17 +77,24 @@ return new class extends Migration
             $table->decimal('cuotaTotal', 10, 2);
             $table->decimal('importeTotal', 10, 2);
             $table->string('primerRegistro');
+
             // Registro adicional
-            $table->string('huellaAnterior');
+
             $table->string('fechaHoraHusoGenRegistro');
             $table->string('numRegistroAcuerdoFacturacion');
             $table->string('idAcuerdoSistemaInformatico');
-            $table->string('tipoHuella');
+            $table->string('tipoHuella')->default('01');
+            $table->string('huella');
             //Identificación nuestra empresa
+            $table->string('nombreFabricanteSoftware')->default('SAUBER OFIMATICA S.L.');
+            $table->string('nifFabricanteSoftware')->default('B53816435');
             $table->string('nombreSoftware');
-            $table->string('versionSoftware');
-            $table->string('nombreFabricanteSoftware');
             $table->string('identificadorSoftware');
+            $table->string('versionSoftware');
+            $table->string('numeroInstalacion');
+            $table->string('tipoUsoPosibleVerifactu')->default('S');
+            $table->string('tipoUsoPosibleMultiOT')->default('S');
+            $table->string('indicadorMultiplesOT')->default('S');
 
             $table->string('enviados')->default('pendiente');
             $table->text('error')->nullable();
