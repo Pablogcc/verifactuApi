4f114a80c26469be86e5a0375d0294280f916c24
Pablo <pablogc2233@gmail.com>
2025-07-29 16:58:18 +0200

    Token puesto en el body al convertir los certificados

--- app/Http/Controllers/CertificadosController.php
+++ app/Http/Controllers/CertificadosController.php
@@ -13,30 +13,33 @@ class CertificadosController extends Controller
 {
     public function convertir(Request $request)
     {
+
         //Recibo por el body el cif del emisor para recoger su certificado
         $data = $request->validate([
-            'cif' => 'required|string'
+            'cif' => 'required|string',
+            'token' => ['required', 'string', 'in:sZQe4cxaEWeFBe3EPkeah0KqowVBLx']
         ]);
 
         //Recogemos todos los campos del cif recibido por el body
         $emisor = Emisores::where('cif', $data['cif'])->get();
 
-      
+
         //return $emisor[0]['certificado'];
 
-          try {
-                    $desencriptador = new Encriptar();
+        try {
+            $desencriptador = new Encriptar();
 
-                    $contrasenna = $desencriptador->decryptString($emisor[0]['password']);
-                    
+            $contrasenna = $desencriptador->decryptString($emisor[0]['password']);
 
-                    $desencriptador->decryptBase64AndDownloadPfx($contrasenna, $emisor[0]['certificado'], $emisor[0]['cif']);
 
-            
-                    ////Ejemplo uso del método encriptar string(Contraseña)
-                    //$contrasenna = $desencriptador->encryptString("Verifactu");
-                } catch (\Throwable $e) {
-                    return $e->getMessage();
-                }
+            $desencriptador->decryptBase64AndDownloadPfx($contrasenna, $emisor[0]['certificado'], $emisor[0]['cif']);
+
+
+            ////Ejemplo uso del método encriptar string(Contraseña)
+            //$contrasenna = $desencriptador->encryptString("Verifactu");
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+
     }
 }
