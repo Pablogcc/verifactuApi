7250d132880bb5df8b3237aea3ac32f6b1855d4c
Pablo <pablogc2233@gmail.com>
2025-08-21 12:32:44 +0200

    Comentarios y comprobación de la encriptación del correo administrativo y del nombre de la empresa

--- app/Http/Controllers/CertificadosController.php
+++ app/Http/Controllers/CertificadosController.php
@@ -14,7 +14,7 @@ class CertificadosController extends Controller
 
         $fechaValidez = null;
 
-        //Recibo por el body el cif del emisor para recoger su certificado
+        //Recibo por el body el cif del emisor, el certificado, la contraseña del certificado, el correo de la empresa, el nombre de la empresa y el token
         $data = $request->validate([
             'cif' => 'required|string',
             'certificado' => 'required|string',
@@ -25,50 +25,56 @@ class CertificadosController extends Controller
         ]);
 
         try {
+            //Llamamos al servicio de encriptar
+            //Usamos los métodos de desencriptar, para desencriptar la contraseña y el certificado para convertirlo en .pem
             $desencriptador = new Encriptar();
 
             $contrasenna = $desencriptador->decryptString($data['password']);
 
-            /*return response()->json([
-                'contraseña_desencriptada' => $contrasenna
-            ]);*/
-
             $paths = $desencriptador->decryptBase64AndDownloadPfx($contrasenna, $data['certificado'], $data['cif']);
 
+            //Obtenemos el certificado en .pem para poder buscar la fecha de validez y el cif del emisor
             $certContent = file_get_contents($paths['cert']);
             if ($certContent === false) {
                 throw new \Exception("No se pudo leer cert.pem");
             }
 
-            // Convertir en recurso de certificado
+            // Devuelve en objeto el certificado
             $cert = openssl_x509_read($certContent);
             if ($cert === false) {
                 throw new \Exception("No se pudo interpretar el certificado");
             }
 
-            // Extraer info
+            // Extraermos la información de la fecha de validez del certificado
+            //La guardamos para luego subirla a la base de datos
             $certInfo = openssl_x509_parse($cert);
             if (!$certInfo || !isset($certInfo['validTo_time_t'])) {
                 throw new \Exception("No se pudo obtener la fecha de validez del certificado");
             }
 
-            // Guardar fecha de caducidad
             $fechaValidez = date('Y-m-d', $certInfo['validTo_time_t']);
 
+            //También extraemos el cif del emisor, para comprobar si es el mismo que el correspondiente
             $nifCertificado = $certInfo['subject']['serialNumber'] ?? null;
 
             if (!$nifCertificado) {
                 throw new \Exception("El certificado no contiene un NIF/CIF válido");
             }
 
-            // Limpiar prefijo (ej. "IDCES-48456925L" → "48456925L")
+            // Limpiar prefijo que tiene al principio para que solo quede el cif y se pueda comparar bien
+            // ej. "IDCES-12345678L" -> "12345678L"
             $nifCertificadoLimpio = preg_replace('/^[A-Z\-]+/', '', $nifCertificado);
 
-            // Comparar NIF/CIF recibido con el del certificado
+            // Aquí comparamos NIF/CIF recibido con el del certificado
+            // Lo ponemos todo en mayúsculas y le quitamos los espacios innecesarios
             if (strtoupper(trim($nifCertificadoLimpio)) !== strtoupper(trim($data['cif']))) {
                 throw new \Exception("El NIF no coincide con el del certificado digital");
             }
 
+            // Comprobamos la fecha actual
+            // Comparamos la fecha actual con la fecha de validez del certificado para saber si está caducado o no, si está caducada no se subirá el certificado digital y saldrá un error
+            // También comprobamos que si le faltan menos de 20 días para caducarse, entonces lo aceptará con un mensaje avisando de que está a punto de caducar
+            //Si le faltan mas de 20 días, entonces se aceptará sin ningún problema
             $hoy = new DateTime();
             $fechaExpira = new \DateTime($fechaValidez);
             $diasRestantes = (int)$hoy->diff($fechaExpira)->format('%r%a');
@@ -77,7 +83,7 @@ class CertificadosController extends Controller
                 throw new \Exception("El certificado ya ha caducado el $fechaValidez");
             }
 
-            $emisor = Emisores::updateOrCreate(
+            Emisores::updateOrCreate(
                 ['cif' => $data['cif']],
                 [
                     'certificado'          => $data['certificado'],
@@ -104,6 +110,7 @@ class CertificadosController extends Controller
             //Ejemplo uso del método encriptar string(Contraseña)
             //$contrasenna = $desencriptador->encryptString("Verifactu");
         } catch (\Throwable $e) {
+            //Si hay algún error(cif incorrecto o fecha validez caducada) entonces no se pasará el certificado digital
             return response()->json([
                 'validado' => false,
                 'fechaValidez' => $fechaValidez,
